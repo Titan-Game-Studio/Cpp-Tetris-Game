bf96d083fe1579516af853e6876131e5da239b0f
Titan Tran <t.p.tai93@gmail.com>
2025-03-16 13:00:50 +0700

    * refractor colors

--- grid.cpp
+++ grid.cpp
@@ -1,5 +1,6 @@
 #include "grid.h"
 #include  <iostream>
+#include "colors.h"
 
 Grid::Grid()
 {
@@ -50,20 +51,4 @@ void Grid::Draw()
 			DrawRectangle(posX, posY, width, height, color);
 		}
 	}
-}
-
-std::vector<Color> Grid::GetCellColors()
-{
-	Color darkGrey = { 26, 31, 40, 255 };
-	Color green = { 47, 230, 23, 255 };
-	Color red = { 232, 18, 18, 255 };
-	Color orange = { 226, 116, 17, 255 };
-	Color yellow = { 237, 234, 4, 255 };
-	Color purple = { 166, 0, 247, 255 };
-	Color cyan = { 21, 204, 209, 255 };
-	Color blue = { 13, 64, 216, 255 };
-	Color lightBlue = { 59, 85, 162, 255 };
-	Color darkBlue = { 44, 44, 127, 255 };
-
-	return { darkGrey, green, red, orange, yellow, purple, cyan, blue };
 }
