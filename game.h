7968f3364a44f7895028a0ba4d8d88837deba3aa
Titan Tran <t.p.tai93@gmail.com>
2025-03-18 21:14:12 +0700

    + add game

---
+++ game.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "grid.h"
+#include "block.h"
+#include "blocks.cpp"
+
+class Game
+{
+public:
+	Game();
+	Block GetRandomBlock();
+	std::vector<Block> GetAllBlocks();
+	void Draw();
+	Grid grid;
+
+private:
+	std::vector<Block> blocks;
+	Block currentBlock;
+	Block nextBlock;
+};
+
